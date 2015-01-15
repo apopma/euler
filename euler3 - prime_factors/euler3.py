2fcac62664c54221b5b3022ea379b15af0ff4ffb
apopma <adam.popma@gmail.com>
2015-01-15 15:13:47 -0600

    Finish euler3
    now much simpler

--- euler3 - prime_factors/euler3.py
+++ euler3 - prime_factors/euler3.py
@@ -1,79 +1,44 @@
-import concurrent.futures
-import math
-
 __author__ = 'apothem'
 
-""" Project Euler problem #3: prime factors
+"""
+Project Euler problem #3: prime factors
 The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143?
 """
 
-''' Pseudocode:
-Accept a target to find the biggest prime factor of
-Generate a list of all prime numbers, from the biggest prime <= target, to 2
-
-Test each prime from biggest to smallest, to see if it's a factor of target
-The first prime that IS a factor of the target is the biggest prime factor! Return it.
-'''
 
 global_target = 600851475143                                                    # ugly but it works
 
-def is_prime(n):                                                                # better prime checker code
-    if n % 2 == 0:                                                              # doesn't mark 2 as prime,
-        return False                                                            # but for this application it's OK
+def is_prime(n):
+    import math                                                                 # better prime checker code
+    if n == 1: return False                                                     # sqrt bit doesn't get 1 or 2 right
+    if n == 2: return True
+    if n % 2 == 0: return False
 
     sqrt_n = int(math.floor(math.sqrt(n)))
     for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
+        if n % i == 0: return False
     return True
 
+# 6857
+def euler3():
+    X = 2
+    candidate = 0
+    biggest_prime_factor = 0
+    prime_factors = []
 
-def make_primes(target, span):
-    print ("Running prime maker...\n")
-    PRIMES = [x for x in range(target, (target - span), -1)]                    # still a bad idea
+    while X < global_target:
+        if is_prime(X):
+            candidate = X
+        if global_target % candidate == 0:
+            prime_factors.append(int(candidate))
+            if candidate > biggest_prime_factor:
+                biggest_prime_factor = int(candidate)
+                print "Biggest prime factor so far is %s" % biggest_prime_factor
+        X += 1
 
-    primeslist = open('primes.txt', 'w')
-    with concurrent.futures.ProcessPoolExecutor() as executor:                  # black sorcery from the Python docs
-        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):       # breaks if it isn't executed using
-            if prime:                                                           # if __name__ == '__main__':
-                primeslist.write(str(number) + "\n")
-                #print ("Found prime: {}".format(number))
-    primeslist.close()
-    print ("Made primes.")
+        if X % 10000000 == 0:
+            print "Searched up to %s" % X
 
+    return biggest_prime_factor
 
-def find_bpf(target, span):
-    found_bpf = False
-
-    print ("Finding biggest prime factor of {:d} with span {:d}...".format(target, span))
-    make_primes(target=target, span=span)
-    print ("Running factor tests...")
-
-    with open('primes.txt', 'r') as infile:
-        for line in infile.readlines():
-            candidate = int(line)
-            if global_target % candidate == 0:
-                print ("Found the biggest prime factor: {:d}!".format(candidate))
-                found_bpf = True
-                return found_bpf
-        if not found_bpf:
-            print ("Couldn't find the biggest prime factor. :(\n")
-            found_bpf = False
-            return found_bpf
-
-
-if __name__ == '__main__':
-    target = math.floor(global_target / 2)
-    span = 100000
-    new_target = target
-    count = 0
-    print ("Biggest Prime Factor Finder 9000:")
-
-    done = find_bpf(target=global_target, span=span)
-    count += 1
-    while not done:
-        new_target = (new_target - span)
-        print ("Searching again with new target {:d} and span {:d}.".format(new_target, span))
-        print ("Searched {:d} times so far, for numbers from {:d} to {:d}.".format(count, global_target, (global_target - target - (count * span))))
-        done = find_bpf(target=new_target, span=span)
-        count += 1
+print euler3()
